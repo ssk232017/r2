f007dc8d26c22e4ac069755c475e816955fe561f
ssk232017 <94100131+ssk232017@users.noreply.github.com>
2021-11-16 23:33:25 +0530

    first commit

---
+++ pattern.java
@@ -0,0 +1,17 @@
+//package testJavaProject;
+import java.util.*;
+
+public class pattern {
+	public static void main(String [] args) {
+		int i=0;
+		for(i=0 ; i<10; i++) {
+			for(int j=i; j>=0; j--) {
+
+				System.out.print("*");
+			}
+			System.out.println(+(i+1)+"\n");
+		}
+	System.out.println("This is pattern Job running on Master from REMOTR Git Repository ssk232017@gmail.com");
+	}
+
+}
